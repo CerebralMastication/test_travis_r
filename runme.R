a0e773fb2ce4532703c95b3504b309c1e2d6d69b
JD Long <jal@renre.com>
2019-06-23 20:16:43 -0400

    checking it in

---
+++ runme.R
@@ -0,0 +1,22 @@
+
+library(tidyverse)
+
+## example ggplot from http://r-statistics.co/Top50-Ggplot2-Visualizations-MasterList-R-Code.html
+
+options(scipen=999)  # turn-off scientific notation like 1e+48
+theme_set(theme_bw())  # pre-set the bw theme.
+data("midwest", package = "ggplot2")
+
+# Scatterplot
+gg <- ggplot(midwest, aes(x=area, y=poptotal)) + 
+  geom_point(aes(col=state, size=popdensity)) + 
+  geom_smooth(method="loess", se=F) + 
+  xlim(c(0, 0.1)) + 
+  ylim(c(0, 500000)) + 
+  labs(subtitle="Area Vs Population", 
+       y="Population", 
+       x="Area", 
+       title="Scatterplot", 
+       caption = "Source: midwest")
+
+plot(gg)
